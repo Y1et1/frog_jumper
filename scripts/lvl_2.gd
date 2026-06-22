6f7701886cab119114734d29aa1205b9a66c8333
Y1et <pandurnoel98@gmail.com>
2026-06-22 20:50:35 +0200

    Add wintimer functionality to level transitions in lvl_1 and lvl_2

--- scripts/lvl_2.gd
+++ scripts/lvl_2.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
+@onready var wintimer: Timer = $wintimer
 
 
 func _on_win_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scenes/lvl_3.tscn")
+		wintimer.start()
+		
 
 
 
 func _on_deathzone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
+
+
+func _on_wintimer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/lvl_3.tscn")
