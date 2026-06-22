62008d8a41809f6328c4ccc1c9447258c6a97807
Y1et <pandurnoel98@gmail.com>
2026-06-22 20:40:04 +0200

    1.0

---
+++ scripts/lvl_1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+func _on_win_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scenes/lvl_2.tscn")
+
+
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_power_bar: ProgressBar = $power_bar/jump_power_bar
+
+var JUMP_VELOCITY = 0
+var JUMP_POWER = 0
+const power_speed = 350
+
+func _physics_process(delta: float) -> void:
+	
+	jump_power_bar.value = abs(JUMP_VELOCITY)
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animated_sprite_2d.frame = 4
+	
+	if is_on_floor():
+		velocity.x = 0
+		animated_sprite_2d.play("landed")
+
+	# Handle jump.
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		
+		JUMP_POWER += power_speed * delta
+		JUMP_POWER = min(JUMP_POWER, 180)
+		
+		JUMP_VELOCITY -= power_speed * delta
+		JUMP_VELOCITY = min(JUMP_VELOCITY, 750)
+		
+	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = JUMP_POWER
+		animated_sprite_2d.play("jump")
+		JUMP_VELOCITY = 0
+		JUMP_POWER = 0
+		
+	move_and_slide()
+	
